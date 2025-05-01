dd90d8a3ed08ea0807bdebab324424bb1090169a
shmador <dorattar4@gmail.com>
2025-05-01 22:18:11 +0300

    Added instance that will launch on the new vpc with custom userdata

--- main.tf
+++ main.tf
@@ -101,3 +101,76 @@ resource "aws_route_table_association" "private2" {
   subnet_id = aws_subnet.private2.id
   route_table_id = aws_route_table.nat.id
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "tls_private_key" "deployer" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = tls_private_key.deployer.public_key_openssh
+}
+
+resource "local_file" "deployer_private_key" {
+  content  = tls_private_key.deployer.private_key_pem
+  filename = "${path.module}/deployer-key.pem"
+
+  file_permission = "0400"
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.public.id
+  associate_public_ip_address = true
+  key_name      = aws_key_pair.deployer.key_name
+
+  vpc_security_group_ids = [aws_security_group.ec2.id]
+
+  user_data = file("${path.module}/userdata")
+
+  tags = {
+    Name = "web-public"
+  }
+}
+
+resource "aws_security_group" "ec2" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh-sg"
+  }
+}
